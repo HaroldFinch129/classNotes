94e485d4f6cdb3323147127fbd69df6d29cbcb1b
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-09 23:02:31 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import EventBind from './components/eventBinding/EventBind'
+// import EventBind from './components/eventBinding/EventBind'
 // import MouseEvents from './components/mouseEvents/MouseEvents'
 // import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
-// import Form from './components/form/Form'
+import Form from './components/form/Form'
 import './App.css'
 
 const App = () => {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Events in React < /h1> { /* <EventBind /> */ } { /* <MouseEvents /> */ } { /* <KeyboardEvents /> */ } { /* <Form /> */ } <
-        /div>
+    return (
+        <div className="App">
+            {/* <h1>Events in React</h1> */}
+            {/* <EventBind /> */}
+            {/* <MouseEvents /> */}
+            {/* <KeyboardEvents /> */}
+            <Form />
+        </div>
     )
 }
 
