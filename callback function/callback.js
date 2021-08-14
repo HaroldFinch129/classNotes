cb80284ed9a2e25be75d1331551876de47c66944
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-14 13:44:20 +0300

    Add files via upload

---
+++ callback function/callback.js
@@ -0,0 +1,60 @@
+// const movements = [100, 300, -200, 500, -400, 225, 1000, -700];
+// let deposit = 0;
+// let witdrow = 0;
+// let arrDeposited = [];
+// let arrWithdraws = [];
+// const toplam = movements.forEach(element => {
+
+//     if (element > 0) {
+//         deposit += element;
+//         arrDeposited.push(element);
+
+//     } else {
+//         witdrow += element
+//         arrWithdraws.push(element);
+//     }
+
+// });
+// console.log(deposit, witdrow);
+// console.log(arrDeposited, arrWithdraws)
+
+// map initials
+const str = 'Clarusway Online Career IT Training School';
+
+const myArr = str.split(" ");
+console.log(myArr);
+
+let sumStr = "";
+myArr.map((item) => {
+    sumStr += item[0]
+
+})
+console.log(sumStr);
+
+
+// let newStr = "";
+// const str = 'The Quick Brown Fox'; // tHe YeLLoW fOx 
+
+
+
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i].toUpperCase() === str[i]) {
+
+//         newStr += str[i].toLowerCase();
+
+//     } else if (str[i].toLowerCase() === str[i]) {
+//         newStr += str[i].toUpperCase();
+//     }
+
+// }
+// console.log(newStr);
+
+
+// 1
+// You deposited $xx
+// You withdrew $xxx 
+
+//  arrDeposits =
+//  arrWithdraws =
+// for each
+//
