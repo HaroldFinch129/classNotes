d6fe185177fe9ec397c4199ae2759485762b1e39
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-16 22:32:03 +0300

    Add files via upload

--- commitfiles/Counter.js
+++ commitfiles/Counter.js
@@ -9,12 +9,32 @@ export default class Counter extends Component {
         }
         console.log("constructor");
     };
+
+    increase = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    static getDerivedStateFromProps(props, state) {
+
+        console.log("getDerivedStateFromProps", state);
+        return null
+    }
+
+    componentDidMount = () => {
+        console.log("Component mounted!");
+        console.log("-----------------")
+    }
+    componentDidUpdate = () => {
+        console.log("Updated");
+    }
     render() {
+        console.log("render")
         return ( <
             div className = "container" >
             <
-            p > counter: < /p> <
-            button > ARTTIR < /button> <
+            p > counter: { this.state.count } < /p>  <
+            button onClick = {
+                () => this.increase() } > ARTTIR < /button>  <
             button > AZALT < /button>
 
             <
