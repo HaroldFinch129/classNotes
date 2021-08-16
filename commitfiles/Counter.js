6b16e4e5a56c06c8d2a4e583b33d18da843efa27
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-16 21:39:48 +0300

    Add files via upload

---
+++ commitfiles/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            p > counter: < /p>
+
+            <
+            /div>
+        )
+    }
+}
