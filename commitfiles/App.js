668fef0a5d478fe4868becb0d94624198ed07816
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-16 21:54:05 +0300

    Add files via upload

--- commitfiles/App.js
+++ commitfiles/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './commitfiles/Counter';
 
 function App() {
+
     return ( <
         div className = "App" >
         <
         h2 > My Counter < /h2> <
+        Counter / >
+        <
         /div>
     );
 }
