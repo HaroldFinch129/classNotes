27658e01167031e6f9f4a2b72a9204ec18efecaf
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-16 21:53:38 +0300

    Add files via upload

--- commitfiles/Counter.js
+++ commitfiles/Counter.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-
+import "./CounterStyle.css"
 export default class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Ali",
+            count: 0,
+        }
+        console.log("constructor");
+    };
     render() {
         return ( <
             div className = "container" >
             <
-            p > counter: < /p>
+            p > counter: < /p> <
+            button > ARTTIR < /button> <
+            button > AZALT < /button>
 
             <
             /div>
