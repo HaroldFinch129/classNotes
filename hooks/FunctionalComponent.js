77328eeb672ca9e1187015d8ce44e8c0cbab4632
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-18 21:35:21 +0300

    Add files via upload

--- hooks/FunctionalComponent.js
+++ hooks/FunctionalComponent.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const FunctionalComponent = () => {
     const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("useEffect");
+        return () => { console.log("func. comp. willunmount"); }
+    }, [counter]);
     const increase = () => {
         setCounter(counter + 1);
     }
@@ -9,9 +14,7 @@ const FunctionalComponent = () => {
         <
         h2 > Functional Component < /h2> <
         p > Counter: { counter } < /p> <
-        button onClick = {
-            () => increase() }
-        Increse > Increase < /button> <
+        button onClick = { increase } > Increase < /button> <
         /div>
     )
 };
