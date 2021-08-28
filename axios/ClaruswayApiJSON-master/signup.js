0c0fc2aa26b8ec200bd426d0469d1f6b605187d7
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-28 11:55:33 +0300

    Add files via upload

---
+++ axios/ClaruswayApiJSON-master/signup.js
@@ -0,0 +1,9 @@
+$ { document }
+ready(function() {
+    setTimeout(fillFormInput, 1000);
+})
+
+const fillFormInput = () => {
+    let email = document.getElementById("email").
+    value = "eve.holt@reqres.in";
+}
