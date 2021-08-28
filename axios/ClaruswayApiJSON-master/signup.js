d373290ff26e22f5446287fec45dc26eb7599d9a
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-28 11:57:01 +0300

    Add files via upload

--- axios/ClaruswayApiJSON-master/signup.js
+++ axios/ClaruswayApiJSON-master/signup.js
@@ -1,9 +1,18 @@
-$ { document }
-ready(function() {
+$(document).ready(function() {
     setTimeout(fillFormInput, 1000);
 })
 
 const fillFormInput = () => {
-    let email = document.getElementById("email").
-    value = "eve.holt@reqres.in";
+    let email = document.getElementById('email').value = "eve.holt@reqres.in";
+    let password = document.getElementById('password').value = "pistol";
+    localStorage.setItem("email", email);
+
+}
+const apiRegister = () => {
+    let email = localStorage.getItem.apply("email"); // Aynı aşağıdaki yöntem ile
+    let password = document.getElementById("pasword").value;
+    const bodyData = {
+        email: email,
+        password: password
+    }
 }
