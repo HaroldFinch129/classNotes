c8f0cb9271d7004b80607f433183975e54175923
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-01 20:41:27 +0300

    Add files via upload

--- hooks/FunctionalComponent.js
+++ hooks/FunctionalComponent.js
@@ -1,20 +1,39 @@
-import React, { useState, useEffect } from 'react';
-const FunctionalComponent= () => {
+import { useState, useRef } from "react";
+const FunctionalComponent = () => {
+    const inputRef = useRef(null);
     const [counter, setCounter] = useState(0);
-
-    useEffect(()=>{
-        console.log("useEffect");
-        return () => {console.log("func. comp. willunmount");}
-    }, [counter]);
+    const [text, setText] = useState("");
+    // useEffect(() => {
+    //   console.log("useEffect");
+    //   return () => {console.log("func. comp. unmount")}
+    // },[counter])
+    // console.log("Func Comp Rendered!")
     const increase = () => {
-        setCounter(counter +1);
-    }
-    return (
-        <div className='function'>
-            <h2>Functional Component</h2>
-            <p>Counter: {counter}</p>
-            <button onClick={increase}>Increase</button>
-        </div>
-    )
+        setCounter(counter + 1);
+    };
+    const changeBGColor = () => {
+        console.log(inputRef.current);
+    };
+    const handleText = (e) => {
+        setText(e.target.value);
+    };
+    return ( <
+        div className = "function" >
+        <
+        h2 > Functional Component < /h2> <
+        p > Counter: { counter } < /p> <
+        button onClick = {
+            () => increase() } > Increase < /button> <
+        hr / >
+        <
+        input ref = { inputRef }
+        type = "text"
+        value = { text }
+        onChange = { handleText }
+        /> <
+        button onClick = {
+            () => changeBGColor() } > BG Color < /button> <
+        /div>
+    );
 };
 export default FunctionalComponent;
