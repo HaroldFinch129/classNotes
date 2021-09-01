908843623eff78d39d4d5b4ceb1205c5181a520e
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-01 20:42:41 +0300

    Add files via upload

--- hooks/App.js
+++ hooks/App.js
@@ -1,36 +1,12 @@
-// life-cycling---------
-
-// import './App.css';
-// import Counter from './commitfiles/Counter';
-
-// function App() {
-  
-//   return (
-//     <div className="App">
-//      <h2>My Counter</h2>
-//      <Counter/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useState } from 'react';
-import FunctionalComponent from './hooks/FunctionalComponent';
-import ClassComponent from './hooks/ClassComponent';
+// import { useState } from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
 
 function App() {
-  const [isVisible, setVisibile] = useState(true);
-  return (
-    <div className="App">
-      <button onClick= {()=> setVisibile(!isVisible)}>Toggle</button>
-      {true && <ClassComponent/>}
-      {isVisible ? <FunctionalComponent/> : null}
-      
-      {/* <ClassComponent/> */}
-      {/* <FunctionalComponent/> */}
-    </div>
-  );
+    // const [isVisible, setVisible] = useState(true);
+    return ( <
+        div className = "App" > { /* <button onClick={() => setVisible(!isVisible)}>Toggle</button> */ } { false && < ClassComponent / > } { true ? < FunctionalComponent / > : null } <
+        /div>
+    );
 }
-
-export default App;
+export default App
