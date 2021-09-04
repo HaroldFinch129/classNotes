2354a50cce68efb522750e18a4b2b71cc0dae29f
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-04 13:41:45 +0300

    Add files via upload

---
+++ context/StudentItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const StudentItem = ({ student, changeColor }) => {
+    return ( <
+        div style = {
+            { background: student.color, paddingBottom: "2rem", paddingTop: "1rem", marginBottom: "1rem" } } >
+        <
+        h3 > { student.name } <
+        /h3> <
+        p >
+        Email: { student.email } <
+        /p> <
+        p >
+        Age: { student.age } <
+        /p>
+        Color: < input type = "text"
+        name = ""
+        id = ""
+        value = { student.color }
+        onChange = {
+            (e) => changeColor(student.id, e.target.value) }
+        /> <
+        /div>
+    )
+}
+
+export default StudentItem
---
+++ context/StudentList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import StudentItem from "./StudentItem";
+const StudentList = ({ students, changeColor }) => {
+    return ( <
+        >
+        <
+        h2 > Student List < /h2> {
+            students.map((student) => ( <
+                StudentItem key = { student.name }
+                student = { student }
+                changeColor = { changeColor }
+                />
+            ))
+        } <
+        />
+    );
+};
+export default StudentList;
