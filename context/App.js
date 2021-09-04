9d0cfc45521fc9fef9a08042bda748c50e52d08f
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-04 13:46:46 +0300

    Add files via upload

--- context/App.js
+++ context/App.js
@@ -1,46 +1,39 @@
-import { useState } from "react";
+import { useState } from 'react';
 import StudentList from "./components/StudentList";
-const data = [{
-        id: 1,
-        name: "Jason Response",
-        email: "jason@mail.com",
-        age: 23,
-        color: "lightcyan",
-    },
-    {
-        id: 2,
-        name: "Benjamin Evalent",
-        email: "benjamin@mail.com",
-        age: 20,
-        color: "honeydew",
-    },
-    {
-        id: 3,
-        name: "Malcolm Function",
-        email: "malcolm@mail.com",
-        age: 21,
-        color: "mistyrose",
-    },
-];
+import { StudentContext } from "./context/StudentContext";
+
+const data = [
+    { id: 1, name: 'Jason Response', email: 'jason@mail.com', age: 23, color: 'lightcyan' },
+    { id: 2, name: 'Benjamin Evalent', email: 'benjamin@mail.com', age: 20, color: 'honeydew' },
+    { id: 3, name: 'Malcolm Function', email: 'malcolm@mail.com', age: 21, color: 'mistyrose' }
+]
+
 const App = () => {
-    const [students, SetStudents] = useState(data);
+    const [students, setStudents] = useState(data)
+
     const changeColor = (id, color) =>
-        SetStudents(
-            students.map((student) =>
-                student.id === id ? {...student, color: color } : student
-            )
-        );
+        setStudents(students.map((student) =>
+            student.id === id ? {...student, color: color } : student
+        ));
+
     return ( <
+        StudentContext.Provider value = "test" >
+
+        <
         div className = "App" >
         <
         header >
         <
         h1 > Welcome! < /h1> <
-        /header> <
+        /header>
+
+        <
         StudentList students = { students }
         changeColor = { changeColor }
         /> <
-        /div>
-    );
-};
+        /div> <
+        /StudentContext.Provider>
+    )
+}
+
 export default App;
