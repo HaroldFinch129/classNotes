8d6455a3ac496e3a1913cd045d7adc5ec5e705b1
Muhammed Halid GAMZE <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-16 22:21:07 +0300

    Add files via upload

--- registerForm/App.js
+++ registerForm/App.js
@@ -1,78 +1,71 @@
-import React from 'react';
+import React from 'react'
+import '../App.css';
 import { useFormik } from 'formik';
-import "../App.css";
-const SimpleRegister = () => {
-    const formik = useFormik({
-        initialValues:{
-            name:"",
-            email:"",
-            userName:"",
-        },
-        onSubmit:values=>{
-            console.log("Values : ", values);
-        },
-        validate:values => {
-            let errors = {}
-                if (!values.name){
-                    errors.name="Required.Please fill this area"
-                }
-                if (!values.email) {
-                  errors.email = "Required.Please fill this area";
-                } else if (
-                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-                    values.email
-                  )
-                ) {
-                  errors.email = "Invalid email address";
-                }
-                if (!values.userName){
-                    errors.userName="Required.Please fill this area"
-                }
-                return errors
-        }
-    })
-    // console.log("Formik Values : ", formik.values);
-    // console.log("Formik : ", formik);
-    return (
-      <div className="container">
-        <h1>Simple Form</h1>
-        <form className="formStyle" onSubmit={formik.handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          ></input>
-          {formik.errors.name ? (
-            <div className="errorStyle">{formik.errors.name}</div>
-          ) : null}
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          ></input>
-          {formik.errors.email ? (
-            <div className="errorStyle">{formik.errors.email}</div>
-          ) : null}
-          <label htmlFor="userName">User name</label>
-          <input
-            type="text"
-            id="userName"
-            name="userName"
-            onChange={formik.handleChange}
-            value={formik.values.userName}
-          ></input>
-          {formik.errors.userName ? (
-            <div className="errorStyle">{formik.errors.userName}</div>
-          ) : null}
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
+import * as Yup from 'yup';
+const initialValues={
+    name:"",
+    email:"",
+    userName:"",
 }
-export default SimpleRegister;
+const onSubmit=(values)=>{
+    console.log("Values:",values)
+}
+const validationSchema=Yup.object({
+    name:Yup.string()
+    .max(15,"Must be 15 characters or less")
+    .required("Required.Enter name"),
+    userName:Yup.string()
+    .max(15,"Must be 15 characters or less")
+    .required("Required.Enter user name"),
+    email:Yup.string()
+    .email("Invalid email address")
+    .required("Required.Enter email address")
+})
+function SimpleRegister() {
+    const formik=useFormik({ 
+        initialValues,
+        onSubmit,
+        validationSchema
+        // validate:values=>{
+        //     let errors={}
+        //     if (!values.name){
+        //         errors.name="Required.Please fill  area"
+        //     }
+        //     if (!values.email){
+        //         errors.email="Required.Please fill area"
+        //     }
+        //     else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        //        errors.email = 'Invalid email address';}
+        //     if (!values.userName){
+        //         errors.userName="Required.Please fill  area"
+        //     }
+        //     return errors
+        // }
+    })
+    console.log("formik:",formik)
+    return (
+        <div className="container">
+            <h1>Simple Form</h1>
+            <form className="formStyle" onSubmit={formik.handleSubmit}>
+                <label  htmlFor="name">Name</label>
+                <input type="text" id="name" name="name"
+                onChange={formik.handleChange} 
+                value={formik.values.name}
+                onBlur={formik.handleBlur}></input>
+                {formik.touched.name && formik.errors.name ?<div  className="errorStyle">{formik.errors.name} </div> : null}
+                <label  htmlFor="email">Email</label>
+                <input type="text" id="email" name="email"
+                onChange={formik.handleChange} value={formik.values.email}
+                onBlur={formik.handleBlur}></input>
+                 {formik.touched.email && formik.errors.email ?<div  className="errorStyle">{formik.errors.email} </div> : null}
+                <label htmlFor="userName">User name</label>
+                <input type="text" id="userName" name="userName"
+                onChange={formik.handleChange} value={formik.values.userName}
+                onBlur={formik.handleBlur}></input>
+                 {formik.touched.userName &&formik.errors.userName ?<div  className="errorStyle">{formik.errors.userName} </div> : null}
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+export default SimpleRegister
